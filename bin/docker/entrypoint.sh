3eade3b9a023dcbdb4ca0722d7aebfc199f1338f
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-04-06 21:16:51 -0700

    bug fixes

--- bin/docker/entrypoint.sh
+++ bin/docker/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "This image is running 'tail -f /dev/null' so that the image stays alive."
+echo "This image is running an infinite loop so that the image stays alive."
 echo "Consider stopping this instance (Ctrl-C) and running in detached mode by running"
 echo
 echo "    docker compose up -d"
 
-exec tail -f /dev/null
+trap 'echo "Stopping the image..."; exit 0' SIGINT
+
+while true; do
+    sleep 1
+done
+
